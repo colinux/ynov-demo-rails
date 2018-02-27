d7913bf3cda9228a555bdbb6f5aabce945fc371e
Colin Darie <colin@darie.eu>
2018-02-27 14:43:33 +0100

    Added new form & create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,23 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    restaurant_params = params.require(:restaurant).permit(:name, :city_id, :photo)
+
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.valid?
+      @restaurant.save!
+
+      redirect_to restaurant_path(@restaurant)
+    else
+      # réafficher le form avec les erreurs
+      render :new
+    end
+  end
 end
