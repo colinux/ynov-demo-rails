2ad1b0ad4b738aeedca38175e18862289e20cd40
Colin Darie <colin@darie.eu>
2018-02-27 14:20:00 +0100

    Added restaurants#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # resources :restaurants
+  resources :restaurants, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "restaurants#index"
